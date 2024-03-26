7a6ccdac3e39776096db524f97573021ccdeceef
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2024-03-26 23:05:31 +0530

    Add files via upload

---
+++ Quantum_Dynamics_Backend/src/main/java/com/example/Quantum_Dynamics_Backend/QuantumDynamicsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quantum_Dynamics_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumDynamicsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumDynamicsBackendApplication.class, args);
+	}
+
+}
---
+++ Quantum_Dynamics_Backend/src/test/java/com/example/Quantum_Dynamics_Backend/QuantumDynamicsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quantum_Dynamics_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumDynamicsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
